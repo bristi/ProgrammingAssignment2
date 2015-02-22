d0e483a6fc38b990168af2e6b64978fa326c8107
NAME <EMAIL>
2015-02-22 16:10:16 +0100

    Added comments and removed test code

--- cachematrix.R
+++ cachematrix.R
@@ -1,44 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In order to allow caching the inverse of a matrix two functions are 
+## written; One providing a data structure for a matrix containing it's
+## own inverse and one providing functionality for using this data structure.
 
-## Write a short comment describing this function
 
+## Create a special matrix object that allows storing the value of it's
+## own inverse.
 makeCacheMatrix <- function(x = matrix()) {
+  # Default value for inverse
   i <- NULL
+  
+  # Initialize a new object from a given matrix y
   set <- function(y) {
     x <<- y
     i <<- NULL
   }
+  # Return matrix representation
   get <- function() x
+  # Get and set the inverse for this matrix
   setinverse <- function(solve) i <<- solve
   getinverse <- function() i
+  
+  # Data structure internals
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Return the inverse of a matrix defined using the special matrix object.
+## If the inverse of the matrix has not yet been calculated then do this
+## now and cache it in the object.
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  # Get the inverse of the matrix object
   i <- x$getinverse()
+  # If it is already calculated then return this value
   if(!is.null(i)) {
     message("getting cached data")
     return(i)
   }
+  
+  # Get the matrix representation and calculate the inverse
   data <- x$get()
   i <- solve(data, ...)
+  # Cache it in the matrix object and return the calculated inverse
   x$setinverse(i)
   i
 }
 
 
-# Test
 
-A <- matrix(c(3,4,5,6), nrow = 2)
-B <- matrix(c(35,4,54,6,99,32,423,87,1), nrow = 3)
 
-Ac <- makeCacheMatrix(A)
-
-cacheSolve(A)

5bf7fd399a0a7b06f45d0ab3d9c567522921e3f3
NAME <EMAIL>
2015-02-22 15:42:02 +0100

    Basic imitation in place

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,41 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) i <<- solve
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
+
+# Test
+
+A <- matrix(c(3,4,5,6), nrow = 2)
+B <- matrix(c(35,4,54,6,99,32,423,87,1), nrow = 3)
+
+Ac <- makeCacheMatrix(A)
+
+cacheSolve(A)
